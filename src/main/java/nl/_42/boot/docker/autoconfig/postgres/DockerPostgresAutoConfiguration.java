9450e68080cf043e7a1496dd1b3ab8685fcb491b
robert-bor <bor.robert@gmail.com>
2017-02-13 20:50:48 +0100

    v0.6.0
    extra field added to check the number of times the startup verification text must be found.
    
    pass spring.datasource.url to the DockerPostgres bean, so it can determine its port.

--- src/main/java/nl/_42/boot/docker/autoconfig/postgres/DockerPostgresAutoConfiguration.java
+++ src/main/java/nl/_42/boot/docker/autoconfig/postgres/DockerPostgresAutoConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.condition.AllNestedConditions;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -23,19 +24,21 @@ public class DockerPostgresAutoConfiguration {
     public static final String DEPEND_ON_BEAN = "dockerPostgresBean";
 
     @Configuration
-    @EnableConfigurationProperties(DockerPostgresProperties.class)
+    @EnableConfigurationProperties({DataSourceProperties.class, DockerPostgresProperties.class})
     public static class Docker42Configuration {
 
         private final DockerPostgresProperties properties;
+        private final DataSourceProperties dataSourceProperties;
 
-        public Docker42Configuration(DockerPostgresProperties properties) {
+        public Docker42Configuration(DockerPostgresProperties properties, DataSourceProperties dataSourceProperties) {
             this.properties = properties;
+            this.dataSourceProperties = dataSourceProperties;
         }
 
         @Bean
         @Conditional(OnDockerPostgresCondition.class)
         public DockerPostgresBean dockerPostgresBean() {
-            return new DockerPostgresBean(properties);
+            return new DockerPostgresBean(properties, dataSourceProperties);
         }
 
     }
--- src/main/java/nl/_42/boot/docker/autoconfig/postgres/DockerPostgresBean.java
+++ src/main/java/nl/_42/boot/docker/autoconfig/postgres/DockerPostgresBean.java
@@ -6,11 +6,11 @@ import nl._42.boot.docker.postgres.DockerPostgresProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.support.AbstractApplicationContext;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.io.IOException;
 
 /**
  * Responsible for the actual setting up and tearing down of the container. The setting up
@@ -21,12 +21,14 @@ public class DockerPostgresBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(DockerPostgresBean.class);
 
     private final DockerPostgresProperties properties;
-
-    public DockerPostgresBean(DockerPostgresProperties properties) {
-        this.properties = properties;
-    }
+    private final DataSourceProperties dataSourceProperties;
 
     private DockerPostgresContainer postgresContainer;
+
+    public DockerPostgresBean(DockerPostgresProperties properties, DataSourceProperties dataSourceProperties) {
+        this.properties = properties;
+        this.dataSourceProperties = dataSourceProperties;
+    }
 
     @Autowired
     private AbstractApplicationContext applicationContext;
@@ -34,7 +36,7 @@ public class DockerPostgresBean {
     @PostConstruct
     public void postConstruct() throws Exception {
         LOGGER.info(">>> Configuring Docker Postgres");
-        postgresContainer = new DockerPostgresBootSequence(properties).execute();
+        postgresContainer = new DockerPostgresBootSequence(properties, dataSourceProperties).execute();
         applicationContext.registerShutdownHook();
     }
 
